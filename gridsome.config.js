21a2811f74db688f77a610e490847d5b29521481
Stephen Ajulu <alunje73@gmail.com>
2020-04-28 00:32:48 +0300

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: 'Ajulu's MicroBlog',
+  siteDescription: 'Personal MicroBlog for Stephen Ajulu. A place to share thoughts, ideas, tips, tricks and short articles',
 
   templates: {
     Post: '/:title',
