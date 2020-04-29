7a9f903e2422b5b0c545d1026188a9b2e25414a0
Stephen Ajulu <alunje73@gmail.com>
2020-04-29 12:18:51 +0300

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -21,11 +21,72 @@ module.exports = {
       options: {
         typeName: 'Post',
         path: 'content/posts/*.md',
+        route: '/:slug',
+        remark: {
+          plugins: [
+            ['@noxify/gridsome-plugin-remark-embed', {
+              'enabledProviders' : [
+                'Youtube',
+                'Twitter',
+                'Gist',
+                'Codepen',
+                'Giphy',
+                'Spotify'
+              ],
+            }]
+          ]
+        },
         refs: {
           // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
           tags: {
             typeName: 'Tag',
+            route: '/tag/:id',
             create: true
+          }
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-102896008-2'
+      }
+    },
+    {
+      use: 'gridsome-plugin-rss',
+      options: {
+        contentTypeName: 'Post',
+        feedOptions: {
+          title: 'Ajulu s Microblog',
+          description: 'Personal MicroBlog for Stephen Ajulu. A place to share thoughts, ideas, tips, tricks and short articles',
+          feed_url: 'https://microblog.stephenajulu.com/rss.xml',
+          site_url: 'https://microblog.stephenajulu.com'
+        },
+        feedItemOptions: node => ({
+          title: node.title,
+          date: node.date,
+          url: 'https://microblog.stephenajulu.com/' + node.slug,
+          description: node.description
+        }),
+        output: {
+          dir: './static',
+          name: 'rss.xml'
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+        exclude: ['/exclude-me'],
+        config: {
+          '/articles/*': {
+            changefreq: 'weekly',
+            priority: 0.5
+          },
+          '/about': {
+            changefreq: 'monthly',
+            priority: 0.7
           }
         }
       }
@@ -42,5 +103,5 @@ module.exports = {
         '@gridsome/remark-prismjs'
       ]
     }
-  }
+  },
 }
